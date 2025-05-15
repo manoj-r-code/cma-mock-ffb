92744d88cd94c6f2671dd90559be41bec0480313
manoj-r-code <manoj.ravindra.123@gmail.com>
2025-05-15 09:01:47 +0530

    updated new questions and adjusted the timer

--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -12,12 +12,12 @@ const Instructions = () => {
     <div className="instructions-page">
       <h1>📝 CMA Mock Test Instructions</h1>
       <ul>
-        <li>You have 120 minutes to complete the test.</li>
-        <li>There are 68 questions, one per page.</li>
+        <li>You have 60 minutes to complete the test.</li>
+        <li>There are 60 questions, one per page.</li>
         <li>You can use Save & Next, Review & Next, or Clear options.</li>
         <li>
-          The sidebar shows question status: 🟢 Answered, 🟡 Review, ⚪
-          Unvisited.
+          The sidebar shows question status: 🟩 Answered,🔴 Not Answered, 🟣
+          Review, ☑️ Answered and marked for review, ⚪ Unvisited.
         </li>
         <li>
           Once time runs out or you click Submit, your answers will be saved.
